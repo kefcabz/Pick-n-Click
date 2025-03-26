af5eb635a622a7e0213f7d9c6f1a02fedc37d7a6
Duc <thienductranhuu2784@gmail.com>
2025-03-26 09:52:09 -0400

    set up core pages of the app

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,6 +3,22 @@ const handleGetHomePage = (req, res) => {
     return res.render("home.ejs", {name})
 }
 
+const handleGetAdminPage = (req, res) => {
+    const name = "Duc"
+    return res.render("admin.ejs")
+}
+
+const handleGetCustomerPage = (req, res) => {
+    const name = "Duc"
+    return res.render("customer.ejs")
+}
+
+const handleAboutPage = (req, res) => {
+    const name = "Duc"
+    return res.render("about.ejs")
+}
+
 module.exports = {
-    handleGetHomePage
+    handleGetHomePage, handleGetAdminPage,
+    handleGetCustomerPage, handleAboutPage
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { handleGetHomePage } from "../controllers/homeController"
+import { handleGetHomePage, handleAboutPage, handleGetAdminPage, handleGetCustomerPage } from "../controllers/homeController"
 
 const router = express.Router() //express.Router() is like a mini Express application. It helps organize routes separately instead of defining everything directly in app.js or server.js.
                                 //Think of it as a way to manage different routes efficiently.
@@ -9,9 +9,9 @@ const initWebRoutes = (app) => {
     //When a user visits "http://localhost:PORT/", the server responds with "Hello World".
     router.get("/", handleGetHomePage)
 
-    router.get("/about", (req, res) => {
-        return res.send("I'm Duc")
-    })
+    router.get("/about", handleAboutPage)
+    router.get("/customer", handleGetCustomerPage)
+    router.get("/admin", handleGetAdminPage)
 
     return app.use("/", router) //This tells Express to use router for handling requests to /.
                                 // "/" is the start of any URL used for this website project
