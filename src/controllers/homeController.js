a87fa782986fad3de93e16737ee163a4039f93b6
Duc <thienductranhuu2784@gmail.com>
2025-03-26 10:58:33 -0400

    register form

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,21 +4,63 @@ const handleGetHomePage = (req, res) => {
 }
 
 const handleGetAdminPage = (req, res) => {
-    const name = "Duc"
     return res.render("admin.ejs")
 }
 
 const handleGetCustomerPage = (req, res) => {
-    const name = "Duc"
     return res.render("customer.ejs")
 }
 
 const handleAboutPage = (req, res) => {
-    const name = "Duc"
     return res.render("about.ejs")
 }
 
+const handleDirectRegisterForm = (req, res) => {
+    return res.render("register.ejs", {
+        errors: null,
+        form: {}
+    })
+}
+
+const handleRegister = async (req, res) => {
+    const { email, username, password, confirmPass } = req.body;
+    console.log("check: ", email, username, password)
+    let errors = {
+        isValidEmail: true,
+        isValidPhone: true,
+        isValidPassword: true,
+        isValidConfirmPass: true
+    };
+
+    // validating
+    if (!email || !/\S+@\S+\.\S+/.test(email)) {
+        errors.isValidEmail = false;
+    }
+
+    if (!password) {
+        errors.isValidPassword = false;
+    }
+
+    if (password !== confirmPass) {
+        errors.isValidConfirmPass = false;
+    }
+
+    const isValid = Object.values(errors).every(v => v === true);
+
+    if (!isValid) {
+        return res.render("home.ejs", {
+            errors,
+            form: { email, username }
+        });
+    }
+
+    // Call service to save user
+    // await registerNewUser(email, phone, username, password);
+
+    return res.redirect("/home.ejs");
+};
+
 module.exports = {
     handleGetHomePage, handleGetAdminPage,
-    handleGetCustomerPage, handleAboutPage
+    handleGetCustomerPage, handleAboutPage, handleDirectRegisterForm, handleRegister
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,7 @@
 import express from "express"
-import { handleGetHomePage, handleAboutPage, handleGetAdminPage, handleGetCustomerPage } from "../controllers/homeController"
+import { handleGetHomePage, handleAboutPage, handleGetAdminPage, handleGetCustomerPage, handleDirectRegisterForm,
+    handleRegister
+ } from "../controllers/homeController"
 
 const router = express.Router() //express.Router() is like a mini Express application. It helps organize routes separately instead of defining everything directly in app.js or server.js.
                                 //Think of it as a way to manage different routes efficiently.
@@ -12,6 +14,8 @@ const initWebRoutes = (app) => {
     router.get("/about", handleAboutPage)
     router.get("/customer", handleGetCustomerPage)
     router.get("/admin", handleGetAdminPage)
+    router.get("/register", handleDirectRegisterForm)
+    router.post("/register", handleRegister)
 
     return app.use("/", router) //This tells Express to use router for handling requests to /.
                                 // "/" is the start of any URL used for this website project
