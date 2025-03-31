cebd2dac8832fa519a83b7393aede3ee06872891
Duc <thienductranhuu2784@gmail.com>
2025-03-31 00:36:40 -0400

    "[Duc]: cleaning src"

--- src/configs/viewEngines.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const configViewEngine = (app) => {
-    app.use(express.static('./src/public')) // we are telling our app where to access images, css and js files
-    app.set("view engine", "ejs")
-    app.set("views", "./src/views")
-}
-
-export default configViewEngine
